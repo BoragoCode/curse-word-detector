0f7f30f28e1d17df38f1611167f2db4d67ececce
Kyle DuPonte <kduponte@gmail.com>
2017-01-14 22:09:43 -1000

    Final

--- curse-word-detector.py
+++ curse-word-detector.py
@@ -1,4 +1,4 @@
-from urllib.request import urlopen
+import urllib.request
 
 def read_text():
 	quotes = open("movie_quotes.txt")
@@ -7,12 +7,13 @@ def read_text():
 	check_profanity(contents_of_file)
 
 def check_profanity(text_to_check):
-	connection = urlopen("http://www.wdylike.appspot.com/?q=" + text_to_check)
+	query = urllib.parse.urlencode({'q': text_to_check})
+	connection = urllib.request.urlopen("http://www.wdylike.appspot.com/?q=" + query)
 	output = connection.read()
 	connection.close()
-	if "true" in output:
+	if "true" in str(output):
 		print("Curse world Alert!!")
-	elif "false" in output:
+	elif "false" in str(output):
 		print("This document has no curse words.")
 	else:
 		print("Could not scan the document properly.")
